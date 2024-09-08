dc1976856faf194dbb2e2a8738d1f9b02ea1d839
Jash-1th <jaswanthmamilla@gmail.com>
2024-09-08 16:23:31 +0530

    Changes

--- schema.js
+++ schema.js
@@ -7,8 +7,7 @@ module.exports.listingsSchema = Joi.object({
         country : Joi.string().required(),
         location : Joi.string().required(),
         price : Joi.number().required().min(0),
-        image : Joi.string().allow("",null),
-        categorie: Joi.string().required()
+        image : Joi.string().allow("",null)
     }).required(),
 })
 
